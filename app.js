de0b8d4ebebc7f21b75002a35836d974a10780d5
unknown <whyblue123@gmail.com>
2017-09-20 14:47:21 +0530

    New-features done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+function aBigNewFeature() {
+ console.log( 'Writing a lot of code' );
+console.log('Finaly completely done with this new feature');
+};
 function something() {
  console.log( 'I\'m something');
- console.log('Bug fixed');
-}
+};
 
 console.log('hello world');
 something();
+aBigNewFeature();
